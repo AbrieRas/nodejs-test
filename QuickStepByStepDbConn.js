a81e5c0f68173abe12fd95362345d68a013d4ef3
Abrie <45531677+FlamingNightmare@users.noreply.github.com>
2023-03-13 16:50:53 +0200

    Update QuickStepByStepDbConn.js

--- QuickStepByStepDbConn.js
+++ QuickStepByStepDbConn.js
@@ -23,22 +23,22 @@ con.connect(function(err) {
 });
 
 
-// new below ver 1
-var http = require('http');
-var fs = require('fs');
+// // new below ver 1
+// var http = require('http');
+// var fs = require('fs');
 
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./home.html', null, function(error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-}
+// function onRequest(request, response) {
+//     response.writeHead(200, {'Content-Type': 'text/html'});
+//     fs.readFile('./home.html', null, function(error, data) {
+//         if (error) {
+//             response.writeHead(404);
+//             response.write('File not found!');
+//         } else {
+//             response.write(data);
+//         }
+//         response.end();
+//     });
+// }
 
 // http.createServer(onRequest).listen(8000);
 
