a7a0de1d3b14736b129c526eec1d3414dc35d496
Abrie <45531677+FlamingNightmare@users.noreply.github.com>
2023-03-13 16:40:30 +0200

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -9,3 +9,20 @@ router.get("/", async (req, res, next) => {
 });
 
 module.exports = router;
+
+// // new below ver 1
+var http = require('http');
+var fs = require('fs');
+
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('../home.html', null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
