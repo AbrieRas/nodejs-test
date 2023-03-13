63be7effcc45d2ec13547c55e6674d3ec765aee0
Abrie <45531677+FlamingNightmare@users.noreply.github.com>
2023-03-13 16:32:59 +0200

    Update QuickStepByStepDbConn.js

--- QuickStepByStepDbConn.js
+++ QuickStepByStepDbConn.js
@@ -29,7 +29,7 @@ var fs = require('fs');
 
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./home.html', null, function(error, data) {
+    fs.readFile('home.html', null, function(error, data) {
         if (error) {
             response.writeHead(404);
             response.write('File not found!');
